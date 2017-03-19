0c6635824f635ca36c02c3e1e2b54f24c47e923a
peng <peng@peng.com>
2017-03-19 20:06:06 +0800

    start coding on bandit

--- src/Charpter2Bandit.hs
+++ src/Charpter2Bandit.hs
@@ -1,6 +1,31 @@
 module Charpter2Bandit
-    ( someFunc
+    ( Bandit(..)
+    , generateBandit
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Lens
+import Data.List(take, repeat)
+import Data.Random.Normal
+
+data Bandit = Bandit {
+     greedyEpsilon :: Double
+    ,kArms :: Int
+    ,qActions :: [Double]
+    ,nActions :: [Int]
+    ,actionRewards :: [[Double]]
+    }
+
+mklenses ''Bandit
+
+generateBandit :: Int -> [[Dboule]] -> IO Bandit
+generateBandit k rewards =
+  pure Bandit {
+   kArms = k
+  ,qActions = take kArms (repeat 0.0)
+  ,nActions = take kArms (repeat 0)
+  ,actionRewards
+  }
+
+actionValues :: Int -> Int -> IO Double
+actionValues k n avs | k < kArms =  avs !! k !! n
+                     | otherwise = error ("kArms = " ++ show kArms ++ ", but index " ++ show k)
--- test/TestChapter2Bandit.hs
+++ test/TestChapter2Bandit.hs
@@ -1,2 +1,11 @@
+module Main where
+
+import Data.Configurator
+import Data.Random.Normal
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+
+-- mkNormals' :: (Random a, Floating a) => (a, a) -> Int -> [a]
+normalsIO'
