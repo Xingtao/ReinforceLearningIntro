de71a50171fd2164e77608adbf331537890b0db8
peng <peng@peng.com>
2017-11-14 22:35:30 -0800

    trival

--- src/Chapter3MDP.hs
+++ src/Chapter3MDP.hs
@@ -10,6 +10,7 @@ module Chapter3MDP
     , Actions
     ) where
 
+import           Control.Lens (makeLenses, over, element, (+~), (&), (.~), (%~))
 import           Data.List(take, repeat)
 import qualified Data.Map as M
 import           Text.Printf
@@ -27,10 +28,17 @@ data Action = U | D | L | R deriving (Show)
 data Policy = Random | Optiomal deriving (Show)
 type Actions = [Action]
 
-type World = [[Double]]
-type Reward = Double
 type Value = Double
+type Values = [[Double]]
+type Reward = Double
 type State = (Int, Int)
+
+data World = World {
+   _values :: Values
+ , _tableMap :: M.Map (State, Action) (State, Reward)
+}
+
+makeLenses ''World
 
 -- usually list has a constant overhead factor than array, caused by indirections & cache-locality,
 -- factor could be quite big in the order of 100 probably.
@@ -42,40 +50,44 @@ type State = (Int, Int)
 ------------------------------------------------------------------------------------------
 -- World Operations
 
-createWorld :: Int -> [((Int, Int), Double)] -> World
-createWorld size spetials =
-  updateSpecials specials . take size . repeat . take size $ repeat 0.0
+createWorld :: Int -> [(State, Reward)] -> World
+createWorld size spetials = 
+  let initValues = take size . repeat . take size . repeat $ 0.0
+      initTable = M.fromList [((x, y), 0.0) | x <- [0..size-1], y <- [0..size-1]]
+  in  World initValues tableMap
   where
   updateSpecials [] world = world
   updateSpecials (x:xs) world = updateWorldAt 
 
-updateWorldAt :: (Int, Int) -> (Double -> Double) -> World -> World
-updateWorldAt (i, j) f mat
- | (upperRows, thisRow : lowerRows ) <- splitAt i mat
- , (leftCells, thisCell: rightCells) <- splitAt j thisRow
-         = upperRows ++ (leftCells ++ (f thisCell): rightCells) : lowerRows
+updateStateValue :: State -> Value -> Values -> Values
+updateStateValue (i, j) newVal values
+ | (upperRows, thisRow  : lowerRows ) <- splitAt i values
+ , (leftCells, thisCell : rightCells) <- splitAt j thisRow
+         = upperRows ++ (leftCells ++ (newVal : rightCells)) : lowerRows
  | otherwise = error "World out of range"
 
+valueOfState :: Values -> State -> Value
+valueOfState values s = values !! fst s !! snd s
+
 -- output in github markdown format
-showWorld :: World -> String
-showWorld world = 
+showValues :: Values -> String
+showValues values = 
   let cols | length world == 0 = 0
-           | otherwise = length (head world)
+           | otherwise = length (head values)
       alignHeader = (concat . take cols . repeat "|:-----:") ++ "|\n"
-  in  alignHeader ++ (concat $ map showLine world)
+  in  alignHeader ++ (concat $ map showLine values)
   where
   showLine [] = "|\n"
   showLine (x:xs) = "|" ++ (printf ".1f" x :: String) ++ (showLine xs)
--- . | left align | center align  | right align |
--- . | :------------ |:---------------:| -----:|
--- . | col 1         |  text           | $1    |
--- . | col 2 is      | centered        |   $12 |
+
+toAction :: Action -> (Int, Int)
+toAction U = (0, negate 1)
+toAction U = (0, 1)
+toAction U = (negate 1, 0)
+toAction U = (1, 0)
 
 ------------------------------------------------------------------------------------------
 -- Learning 
-
-valueOfState :: World -> State -> Value
-valueOfState world s = world !! fst s !! snd s
 
 -- value of an action in state s
 valueOfAction :: World -> State -> Action -> Value
